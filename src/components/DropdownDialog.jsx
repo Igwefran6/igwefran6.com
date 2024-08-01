790b5b03b10ad08a74f62d0fb8bc3211ce25462c
Igwe Francis <igwefran6@gmail.com>
2024-08-01 12:58:41 +0100

    Add bubble effect

--- src/components/DropdownDialog.jsx
+++ src/components/DropdownDialog.jsx
@@ -8,7 +8,7 @@ const DropdownDialog = () => {
       <div className="flex flex-col gap-2 w-[200px] h-auto bg-primary-transparent p-8 rounded-xl z-10">
         <Link
           to="/"
-          className="hover:ml-2 active:underline underline-offset-2 transition-all "
+          className="hover:ml-2 active:underline underline-offset-2 transition-all active:opacity-50 "
         >
           <span className="hover:after:content-['↗'] transition-all">
             Projects
@@ -16,7 +16,7 @@ const DropdownDialog = () => {
         </Link>
         <Link
           to="/"
-          className="hover:ml-2 active:underline underline-offset-2 transition-all"
+          className="hover:ml-2 active:underline underline-offset-2 transition-all active:opacity-50"
         >
           <span className="hover:after:content-['↗'] transition-all">
             Services
@@ -24,7 +24,7 @@ const DropdownDialog = () => {
         </Link>
         <Link
           to="/"
-          className="hover:ml-2 active:underline underline-offset-2  transition-all"
+          className="hover:ml-2 active:underline underline-offset-2  transition-all active:opacity-50"
         >
           <span className="hover:after:content-['↗'] transition-all">Blog</span>
         </Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,28 +5,32 @@ import SocialIcon from "../components/SocialIcon";
 import ScrolldownMssg from "../components/ScrolldownMssg";
 import { mdiGithub, mdiFacebook, mdiTwitter } from "@mdi/js";
 import AnimatedCursorSetup from "../components/layouts/AnimatedCursorSetup";
+import Bubble from "../components/layouts/Bubbles";
 
 const Home = () => {
   return (
-    <div className="w-screen h-screen overflow-x-hidden relative bg-background">
-      <Header />
-      <div className="absolute left-20 top-1/2 -translate-y-1/2 flex flex-col gap-3 max-lg:hidden">
-        <SocialIcon icon={mdiGithub} toolTip="github" />
-        <SocialIcon icon={mdiFacebook} toolTip="facebook" />
-        <SocialIcon icon={mdiTwitter} toolTip="twitter" />
-      </div>
-      <div className="absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
-        {" "}
-        <IntroContainer />
-        <div className="m-auto w-fit mt-4 ">
-          <Button text="My Projects" />
+    <>
+      <div className="w-screen h-screen overflow-x-hidden relative bg-transparent">
+        <Header />
+        <div className="absolute left-20 top-1/2 -translate-y-1/2 flex flex-col gap-3 max-lg:hidden">
+          <SocialIcon icon={mdiGithub} toolTip="github" />
+          <SocialIcon icon={mdiFacebook} toolTip="facebook" />
+          <SocialIcon icon={mdiTwitter} toolTip="twitter" />
         </div>
+        <div className="absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
+          {" "}
+          <IntroContainer />
+          <div className="m-auto w-fit mt-4 ">
+            <Button text="My Projects" />
+          </div>
+        </div>
+        <div className="absolute right-8 bottom-[120px] translate-x-1/2 max-lg:hidden">
+          <ScrolldownMssg />
+        </div>
+        <AnimatedCursorSetup />
       </div>
-      <div className="absolute right-8 bottom-[120px] translate-x-1/2 max-lg:hidden">
-        <ScrolldownMssg />
-      </div>
-      <AnimatedCursorSetup />
-    </div>
+      <Bubble />
+    </>
   );
 };
 
