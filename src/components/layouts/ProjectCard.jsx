3d9d41e4146e4073671674d33916f4fe8f3473ea
Igwe Francis <igwefran6@gmail.com>
2024-08-03 15:23:30 +0100

    Modify Project and ProjectCard

--- src/components/layouts/ProjectCard.jsx
+++ src/components/layouts/ProjectCard.jsx
@@ -1,42 +1,47 @@
-const ProjectCard = () => {
+import { useEffect } from "react";
+const ProjectCard = ({
+  title,
+  imgURL,
+  text,
+  technologies,
+  demoURL,
+  websiteURL,
+}) => {
+  const gotoURL = (url) => {
+    window.location.href = url;
+  };
   return (
     <div className="flex flex-row h-auto pb-4 opacity-85 hover:opacity-90">
       <div className="w-[35%] bg-slate-800 p-8 text-primary">
-        <h2 className="text-2xl mb-4">Dictle</h2>
-        <p className="pr-4">
-          Dictle is a dictionary app designed to provide users with quick and
-          easy access to definitions, synonyms, antonyms, and example sentences
-          for any word. The app is designed with simplicity and efficiency in
-          mind, making it an ideal tool for students, writers, and anyone
-          looking to expand their vocabulary.
-        </p>
+        <h2 className="text-2xl mb-4">{title}</h2>
+        <p className="pr-4">{text}</p>
         <div className="flex gap-2 flex-wrap">
-          {" "}
-          <span className="p-2 py-1 my-2 border border-primary inline-block">
-            ReactJS
-          </span>
-          <span className="p-2 py-1 my-2 border border-primary inline-block">
-            Tailwind
-          </span>
-          <span className="p-2 py-1 my-2 border border-primary inline-block">
-            Vercel
-          </span>
+          {technologies &&
+            technologies.map((t) => {
+              return (
+                <span className="p-2 py-1 my-2 border border-primary inline-block">
+                  {t}
+                </span>
+              );
+            })}
         </div>
         <div className=" flex gap-2 flex-wrap">
-          <button className="p-2 py-1 my-2 border border-primary inline-block w-[45%] hover:bg-primary hover:text-secondary active:scale-105 font-semibold">
+          <button
+            onClick={() => gotoURL(demoURL)}
+            className="p-2 py-1 my-2 border border-primary inline-block w-[45%] hover:bg-primary hover:text-secondary active:scale-105 font-semibold"
+          >
             Demo
           </button>
-          <button className="p-2 py-1 my-2 border border-primary inline-block w-[45%] hover:bg-primary hover:text-secondary active:scale-105 font-semibold">
+          <button
+            onClick={() => gotoURL(websiteURL)}
+            className="p-2 py-1 my-2 border border-primary inline-block w-[45%] hover:bg-primary hover:text-secondary active:scale-105 font-semibold"
+          >
             Visit
           </button>
         </div>
       </div>
       <div className="w-[65%] flex items-center ">
-        <img
-          src="../../src/assets/images/Dictle-img.png"
-          alt="Project"
-          className="w-full h-auto"
-        />
+        <img src={imgURL} alt="Project" className="w-full h-auto" />
       </div>
     </div>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,7 +5,18 @@ const Project = () => {
       <h1 className="text-4xl text-primary ">Projects</h1>
       <hr className="w-24 border-t-4 my-4 border-text-orange"></hr>
       <div className="">
-        <ProjectCard />
+        <ProjectCard
+          title="Dictle"
+          imgURL="../../src/assets/images/Dictle-img.png"
+          text="Dictle is a dictionary app designed to provide users with quick and
+          easy access to definitions, synonyms, antonyms, and example sentences
+          for any word. The app is designed with simplicity and efficiency in
+          mind, making it an ideal tool for students, writers, and anyone
+          looking to expand their vocabulary."
+          technologies={["ReactJs", "Tailwind", "Vercel"]}
+          demoURL="#"
+          websiteURL="#"
+        />
       </div>
     </div>
   );
