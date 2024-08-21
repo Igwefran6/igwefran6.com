c2ed211bf28c1aff87e6a8fe35d24b2b3d742320
Igwe Francis <igwefran6@gmail.com>
2024-08-21 20:26:55 +0100

    Add email contact functionality with emailJS

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,67 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import validator from "validator";
 import { useScrollContext } from "../hooks/useScrollContext";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const { dispatch } = useScrollContext();
+  const form = useRef();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
   });
-
-  const [errors, setErrors] = useState({});
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [submissionStatus, setSubmissionStatus] = useState("");
+  const [errMssg, setErrMssg] = useState({});
+  const [submitting, setSubmitting] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+    setFormData({ ...formData, [name]: value });
   };
-
-  const validateForm = () => {
+  const inputsValid = () => {
     const { name, email, message } = formData;
-    const newErrors = {};
+    const errors = {};
 
     if (validator.isEmpty(name)) {
-      newErrors.name = "Name is required.";
+      errors.name = "I'd love to know your name!";
     }
-
     if (!validator.isEmail(email)) {
-      newErrors.email = "Invalid email address.";
+      errors.email = "Invalid email address!";
     }
-
     if (validator.isEmpty(message)) {
-      newErrors.message = "Message is required.";
+      errors.message = "Please write me some message!";
     }
 
-    setErrors(newErrors);
-
-    return Object.keys(newErrors).length === 0;
+    setErrMssg(errors);
+    return Object.keys(errors).length === 0;
   };
 
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    if (!validateForm()) {
+    if (!inputsValid()) {
       return;
     }
 
-    setIsSubmitting(true);
-
-    try {
-      setSubmissionStatus("Message sent successfully!");
-      setFormData({ name: "", email: "", message: "" });
-      setErrors({});
-    } catch (error) {
-      setSubmissionStatus("An error occurred. Please try again.");
-    } finally {
-      setIsSubmitting(false);
-    }
+    setSubmitting(true);
+    emailjs
+      .sendForm("service_etijegc", "template_7ilr8bk", form.current, {
+        publicKey: "Jevwt9uRllCInf_FC",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          setSuccess(true);
+          setSubmitting(false);
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          setSubmitting(false);
+        }
+      );
   };
+
+  useEffect(() => {
+    if (success) {
+      const delay = setTimeout(() => {
+        setSuccess(false);
+      }, 3000);
+
+      return () => clearTimeout(delay);
+    }
+  }, [success]);
 
   return (
     <div className="relative w-screen bg-[url('/images/Get-in-touch.jpg')] bg-fixed bg-center bg-no-repeat bg-cover p-20 ">
@@ -69,7 +78,7 @@ const Contact = () => {
       <div className="relative z-10 max-w-md w-full bg-transparent shadow-lg rounded-lg">
         <h2 className="text-4xl mb-6 text-primary">Get In Touch</h2>
         <hr className="w-24 border-t-4 my-4 border-text-orange" />
-        <form onSubmit={handleSubmit} className="space-y-4">
+        <form ref={form} onSubmit={sendEmail} className="space-y-4">
           <div>
             <label
               htmlFor="name"
@@ -81,16 +90,13 @@ const Contact = () => {
               type="text"
               id="name"
               name="name"
-              value={formData.name}
               onChange={handleChange}
-              required
-              className={`mt-1 block w-full border-b-2 border-primary bg-[#00000044] text-primary p-2 pb-1 font-semibold focus:border-b-2 focus:outline-none ${
-                errors.name ? "border-red-500" : "border-gray-300"
-              }`}
+              className={
+                `mt-1 block w-full border-b-2  bg-[#00000044] text-primary p-2 pb-1 font-semibold focus:border-b-2 focus:outline-none ` +
+                (!errMssg.name ? "border-primary" : " border-text-orange")
+              }
             />
-            {errors.name && (
-              <p className="mt-2 text-sm text-red-600">{errors.name}</p>
-            )}
+            <span className="text-text-orange">{errMssg.name}</span>
           </div>
           <div>
             <label
@@ -103,16 +109,13 @@ const Contact = () => {
               type="email"
               id="email"
               name="email"
-              value={formData.email}
               onChange={handleChange}
-              required
-              className={`mt-1 block w-full border-b-2 border-primary bg-[#00000044] text-primary p-2 pb-1 font-semibold focus:border-b-2 focus:outline-none ${
-                errors.email ? "border-red-500" : "border-gray-300"
-              }`}
+              className={
+                `mt-1 block w-full border-b-2  bg-[#00000044] text-primary p-2 pb-1 font-semibold focus:border-b-2 focus:outline-none ` +
+                (!errMssg.email ? "border-primary" : " border-text-orange")
+              }
             />
-            {errors.email && (
-              <p className="mt-2 text-sm text-red-600">{errors.email}</p>
-            )}
+            <span className="text-text-orange">{errMssg.email}</span>
           </div>
           <div>
             <label
@@ -124,27 +127,24 @@ const Contact = () => {
             <textarea
               id="message"
               name="message"
-              rows="4"
-              value={formData.message}
               onChange={handleChange}
-              required
-              className={`mt-1 block w-full border-b-2 border-primary bg-[#00000044] text-primary p-2 pb-1 font-semibold focus:border-b-2 focus:outline-none ${
-                errors.message ? "border-red-500" : "border-gray-300"
-              }`}
+              rows="4"
+              className={
+                `mt-1 block w-full border-b-2  bg-[#00000044] text-primary p-2 pb-1 font-semibold focus:border-b-2 focus:outline-none ` +
+                (!errMssg.message ? "border-primary" : " border-text-orange")
+              }
             />
-            {errors.message && (
-              <p className="mt-2 text-sm text-red-600">{errors.message}</p>
-            )}
+            <span className="text-text-orange">{errMssg.message}</span>
           </div>
           <button
             type="submit"
-            disabled={isSubmitting}
+            disabled={submitting}
             className="text-2xl w-full py-2 px-4 border-4 border-primary  text-primary hover:bg-secondary hover:text-primary font-semibold  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           >
-            {isSubmitting ? "Sending..." : "Send Message"}
+            {submitting ? "Submitting..." : "Send Message"}
           </button>
-          {submissionStatus && (
-            <div className="mt-4 text-sm text-white">{submissionStatus}</div>
+          {success && (
+            <span className="text-primary">Message sent successfully!</span>
           )}
         </form>
       </div>
