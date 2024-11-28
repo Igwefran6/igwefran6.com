c3be9f8b98e725b8d1da5e4408b4cd878aa2ba2f
igwe francis <igwefran6@gmail.com>
2024-11-28 09:39:32 +0100

    --allow-empty

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
   const { dispatch } = useScrollContext();
   return (
     <>
-      <div className="w-screen h-svh overflow-x-hidden relative bg-transparent z-50">
+      <div className="w-screen h-svh overflow-x-hidden relative bg-transparent">
         <Header />
-        <div className="fixed lg:absolute bottom-8 right-1/2 translate-x-1/2 lg:left-20 lg:bottom-1/2 lg:translate-y-1/2 lg:translate-x-0 flex lg:flex-col gap-3 ">
+        <div className="fixed lg:absolute bottom-8 right-1/2 translate-x-1/2 lg:left-20 lg:bottom-1/2 lg:translate-y-1/2 lg:translate-x-0 flex lg:flex-col gap-3 z-50">
           <SocialIcon
             icon={mdiGithub}
             toolTip="github"
